9e3af3ac46026d13c6c53dbb9f6b340537c02f96
PeterBrownMortgageAdvisor <peterbrown.mortgages@hotmail.com>
2026-03-25 14:00:38 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Peter Brown - The Mortgage Advisor',
